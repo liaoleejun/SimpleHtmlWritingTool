20a04c0305b6897e8bd50ab310d73603fd4082fb
Leejun Liao <liaoleejun@163.com>
2019-05-29 16:13:30 +0800

    Add auto numbering of headings
     * 标题编号应该用代码来自动化, 省力
     * 通过js来自动化标题的编号, h2-h6
     * 标题的编号用了灰色来在视觉上弱化
    
    The gains from the development of this commit:
    
     * 把所有能合并的js合并到一个js文件里面, 方便集中管理与统一引用

--- static/script.js
+++ static/script.js
@@ -1,8 +1,34 @@
 $(document).ready(function() {
 
-    // when click image, open image in a new window
+    // When click image, open image in a new window
     $(document.body).on("click", "img", [], function () {
         // window.location.href = this.src;
         window.open(this.src);
     });
+
+    // Auto numbering of headings
+    let indices = [];
+    function addIndex() {
+        // jQuery will give all the HNs in document order
+        jQuery('h2,h3,h4,h5,h6').each(function() {
+            let hIndex = parseInt(this.nodeName.substring(1)) - 2;
+
+            // just found a levelUp event
+            if (indices.length - 1 > hIndex) {
+                indices= indices.slice(0, hIndex + 1 );
+            }
+
+            // just found a levelDown event
+            if (indices[hIndex] === undefined) {
+                indices[hIndex] = 0;
+            }
+
+            // count + 1 at current level
+            indices[hIndex]++;
+
+            // display the full position in the hierarchy
+            jQuery(this).prepend("<span style='color: #ccc;'>" + indices.join(".") + ".&ensp;</span>");
+        });
+    }
+    addIndex();
 });
